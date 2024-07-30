c7652bbd729abbace61ad3165c3df818a417f91f
MDSZilla <mdszilla@gmail.com>
2024-07-30 13:52:52 +0500

    Updated Example Functions to check for Auth Header

--- api/v1/example/GET/exampleGET.ts
+++ api/v1/example/GET/exampleGET.ts
@@ -6,6 +6,11 @@ export async function exampleGET(request: Request, response: Response){
     if(request.method === "GET"){
         //Read Auth Header
         const authHeader = request.headers.authorization;
+
+        if(!authHeader){ //If no Value is in the Authorization Header
+            // return response.status(401).send(new ServerResponse<CreateSpinOutputDTO>(new CreateSpinOutputDTO(), ServerResponseStatus.CLIENTERROR, "Unauthorized Access"));
+        };
+
         if(!verifyAuthentication(authHeader)){ //Check if Authorization header is valid and token is not expired
             response.cookie(process.env.AUTH_COOKIE_NAME!, "", { expires: new Date(0) }); //Clear Cookie
             //Return Response
